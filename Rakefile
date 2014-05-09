744685042de9388b6658f89077e827d846eb7e0e
roughani <ash@publicinnovation.org>
2014-05-09 13:23:31 -0700

    Rename app HNL ✈ SMF

--- Rakefile
+++ Rakefile
@@ -3,4 +3,4 @@
 
 require File.expand_path('../config/application', __FILE__)
 
-Honoluluanswers::Application.load_tasks
+SacramentoAnswers::Application.load_tasks
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Honoluluanswers::Application.config.session_store :cookie_store, key: '_honoluluanswers_session'
+SacramentoAnswers::Application.config.session_store :cookie_store, key: '_sacramento_answers_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Honoluluanswers::Application.config.session_store :active_record_store
+# SacramentoAnswers::Application.config.session_store :active_record_store
